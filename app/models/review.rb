b893cfb88376be91f54a5ef7a4a61894e12c4402
Francois <francois.desazars@gmail.com>
2020-05-13 16:09:11 +0200

    finished yelp copycat

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :restaurant
-  validates :rating, inclusion: { in: [0, 1, 2, 3, 4, 5] }
+  validates :rating, inclusion: { in: [0, 1, 2, 3, 4, 5] }, numericality: { only_integer: true }
   validates :rating, presence: true
   validates :content, presence: true
   validates :restaurant_id, presence: true
